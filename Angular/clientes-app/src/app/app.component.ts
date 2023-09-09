b7ccb10718bfb3d7a15ae7d0aba6516d9d51b04e
andres briñez <brinezlopez08@gmail.com>
2023-09-08 22:16:52 -0500

    Agregar notas para entender más el código

--- Angular/clientes-app/src/app/app.component.ts
+++ Angular/clientes-app/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', // el selector es el nombre de la etiqueta que se va a usar en el html(index.html) y esta etiqueta se va a reemplazar por el contenido del template
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', // es el template que en el que se va a mostrar el contenido del componente
+  styleUrls: ['./app.component.css'] // son los esitlos que se van a aplicar al template (componente) y no a los demas, van en [] porque pueden ser varios separados por ,
 })
 export class AppComponent {
   title = 'clientes-app';
+  nombre = 'Felipe';
 }
