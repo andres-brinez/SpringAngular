ad2cb0476a2ba58a91484e080e83381f86d94681
andres briñez <brinezlopez08@gmail.com>
2023-10-11 23:32:28 -0500

    Implementando Servicio Angular con HttpClient

--- Angular/clientes-app/src/app/app.module.ts
+++ Angular/clientes-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'; // Sirve para registrar los componentes, servicios, directivas, etc. que se van a utilizar en la aplicación
+import { BrowserModule } from '@angular/platform-browser'; // Sirve para que la aplicación se ejecute en el navegador
+import { HttpClientModule } from '@angular/common/http'; //  HttpClient sirve para hacer peticiones HTTP a un servidor y recibir respuestas en diferentes formatos
+import { RouterModule,Routes } from '@angular/router';
 
+//Componentes
 import { AppComponent } from './appComponent/app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
-import { RouterModule,Routes } from '@angular/router';
 
 // Constante con un arreglo de las rutas
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent} // ruta por defecto
 ];
 @NgModule({
   declarations: [
@@ -20,10 +22,12 @@ const routes: Routes = [
     ClientesComponent
   ],
   imports: [
-    BrowserModule
-    ,RouterModule.forRoot(routes) // esto es para registrar las rutas
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes) // esto es para registrar las rutas
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- Angular/clientes-app/src/app/clientes/cliente.service.ts
+++ Angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http'; //  HttpClient sirve para hacer peticiones HTTP a un servidor y recibir respuestas en diferentes formatos
+import { Cliente } from './Cliente';
+import { Observable } from 'rxjs';
+
+/*
+Observable se utiliza con frecuencia para manejar solicitudes HTTP y eventos del usuario.
+Por ejemplo, cuando se realiza una solicitud HTTP con HttpClient, la respuesta del servidor se devuelve como un Observable. 
+También se puede utilizar Observable para manejar eventos del usuario, como clics de botón o cambios de entrada
+.*/
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  //guarda  la URL del recurso donde se encuentra el listado de clientes en una variable
+  private urlEndPoint:string = "http://localhost:8080/api/clientes";
+
+  // Se hace inyección de dependencias de HttpClient en el constructor de la clase para poder utilizarlo
+  constructor(private http:HttpClient ) {}
+
+  // Método que obtiene el listado de clientes
+  getClientes(): Observable<Cliente[]> { 
+    // Se hace la petición GET al servidor y se le pasa la URL del recurso
+    // Se utiliza el método get() de HttpClient para hacer la petición
+    // Hace la petición y se suscribe al Observable para recibir la respuesta
+    // y se convierte la respuesta a un arreglo de clientes porqu3 viene en formato JSON, esto es un casteo <Tipo de dato> 
+    // Se retorna un Observable que contiene un arreglo de clientes
+
+    // Se hace el casteo con map
+    // return this.http.get(this.urlEndPoint).pipe(
+    //   map(response => response as Cliente[])
+    // );
+
+    return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
 }
