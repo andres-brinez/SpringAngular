985a602e5979407282c93b6eb052e6728bc65ffa
andres briñez <brinezlopez08@gmail.com>
2023-10-11 23:32:57 -0500

    se crea la clase que representa los datos a recibir

--- Angular/clientes-app/src/app/clientes/Cliente.ts
+++ Angular/clientes-app/src/app/clientes/Cliente.ts
@@ -1,8 +1,15 @@
-export class Cliente {
-  
-    constructor(){
-        
-    }
-    // constructor(id: number, nombre: string, apellido: string, createAt: string, email: string) {
+export class Cliente { // Se crea la clase Cliente que representa el modelo de datos de un cliente
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    createAt: string;
 
+     constructor(id: number, nombre: string, apellido: string, email: string, createAt: string) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.createAt = createAt;
+     }
 }
