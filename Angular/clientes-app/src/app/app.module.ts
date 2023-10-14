89c5edf2cf2bedf9873bb4d8ee94789c3b83a23c
andres briñez <brinezlopez08@gmail.com>
2023-10-14 18:56:10 -0500

    Creando el componente form.component

--- Angular/clientes-app/src/app/app.module.ts
+++ Angular/clientes-app/src/app/app.module.ts
@@ -1,33 +1,47 @@
 import { NgModule } from '@angular/core'; // Sirve para registrar los componentes, servicios, directivas, etc. que se van a utilizar en la aplicación
 import { BrowserModule } from '@angular/platform-browser'; // Sirve para que la aplicación se ejecute en el navegador
 import { HttpClientModule } from '@angular/common/http'; //  HttpClient sirve para hacer peticiones HTTP a un servidor y recibir respuestas en diferentes formatos
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router'; // Sirve para registrar las rutas
+import { FormsModule } from '@angular/forms'; // Sirve para enlazar los datos del formulario con el modelo
+
 
 //Componentes
 import { AppComponent } from './appComponent/app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
 
 // Constante con un arreglo de las rutas
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'clientes', component: ClientesComponent} // ruta por defecto
 ];
+
+// Decorador que registra los componentes, servicios, directivas, etc. que se van a utilizar en la aplicación
 @NgModule({
-  declarations: [
+  declarations: [ // Aquí se registran los componentes
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
-  imports: [
+  // Aquí se registran los módulos que se van a utilizar en la aplicación
+  imports: [ 
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes) // esto es para registrar las rutas
+    RouterModule.forRoot(routes), // esto es para registrar las rutas
+    FormsModule
+    
+    
+
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent] 
 })
+
 export class AppModule { }
 
---
+++ Angular/clientes-app/src/app/clientes/form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './Cliente';
+//OnInit se utiliza para realizar tareas de inicialización después de que se hayan inicializado las propiedades de entrada del componente.
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+})
+
+
+export class FormComponent implements OnInit {
+
+  private cliente:Cliente = new Cliente(); 
+  private titulo:string = "Crear Cliente";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Metodo que se llama cuando se envia el formulario
+  public onSubmit():void{
+    console.log("Clicked!");
+    console.log(this.cliente);
+  }
+
+}
