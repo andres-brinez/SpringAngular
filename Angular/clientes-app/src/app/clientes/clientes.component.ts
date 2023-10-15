4f1763fbad12beb54f45f762075a9dac4b3c2565
andres briñez <brinezlopez08@gmail.com>
2023-10-15 00:10:41 -0500

    Escribiendo el delete  en el componente clientes

--- Angular/clientes-app/src/app/clientes/clientes.component.ts
+++ Angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import { Component,OnInit } from '@angular/core';
 //El método ngOnInit() se utiliza con frecuencia para realizar tareas de inicialización en un componente, como cargar datos de un servicio o configurar variables de estado. También se puede utilizar para realizar tareas de configuración adicionales después de que se hayan inicializado las propiedades de entrada.
 import { Cliente } from './Cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -23,6 +24,42 @@ export class ClientesComponent implements OnInit {
       // this.clientes=clientes  
       //}
     ); // Se llama al método getClientes() del servicio y se suscribe al Observable para recibir la respuesta que son los clientes y se asignan al array de clientes
+  }
+
+  // El objeto cliente que se recibe como parámetro viene del botón eliminar del componente clientes.component.html
+  delete(cliente:Cliente):void{
+    // Se llama al método delete() del servicio y se suscribe al Observable para recibir la respuesta que es el cliente que se eliminó
+    this.clienteService.delete(cliente.id).subscribe(
+      response => {
+        // swal para preguntar si se quiere eliminar el cliente, are you sure?
+        Swal.fire({
+          title: '¿Estás seguro?',
+          text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '!Sí, eliminar',
+          cancelButtonText: '!No, cancelar'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              this.clienteService.delete(cliente.id).subscribe(
+                response => {
+                  // Se filtra el cliente que se eliminó para que no esté en el array de clientes, esto es para que se actualice la lista de clientes en la vista
+                  this.clientes = this.clientes.filter(cli => cli !== cliente)  // compara cada cliente (cli) y verifica que sea diferente al cliente que se eliminó para agregarlo al array de clientes
+                  Swal.fire(
+                    'Eliminado!',
+                    `Cliente ${cliente.nombre} eliminado con éxito.`,
+                    'success'
+                  )
+                }
+              )
+          }
+        })
+
+
+      }
+    )
   }
 
 }
