9be14a76187475e10a7183272913f9b61b46d61b
andres briñez <brinezlopez08@gmail.com>
2023-10-19 09:40:07 -0500

    Se agrega la ruta para ver el componente de detalle

--- Angular/clientes-app/src/app/app.module.ts
+++ Angular/clientes-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormComponent } from './clientes/form/form.component';
 import { ClienteService } from './clientes/cliente.service';
 import { ClientesPageComponent } from './clientes/clientes-page/clientes-page.component';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { DetalleComponent } from './clientes/detalle/detalle.component';
 
 // Constante con un arreglo de las rutas
 const routes: Routes = [
@@ -21,7 +22,8 @@ const routes: Routes = [
   {path: 'clientes', component: ClientesComponent},
   {path: 'clientes/form', component: FormComponent},
   {path: 'clientes/form/:id', component: FormComponent},
-  {path: 'clientes/page/:page', component: ClientesPageComponent}
+  {path: 'clientes/page/:page', component: ClientesPageComponent},
+  {path: 'clientes/ver/:id', component: DetalleComponent}
 
 ];
 
@@ -34,7 +36,8 @@ const routes: Routes = [
     ClientesComponent,
     FormComponent,
     ClientesPageComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    DetalleComponent
   ],
   // Aquí se registran los módulos que se van a utilizar en la aplicación
   imports: [ 
