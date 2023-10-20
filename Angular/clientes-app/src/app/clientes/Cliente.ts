0e8ab1e3e40719d325703eb5e5432a334b9bcd6e
andres briñez <brinezlopez08@gmail.com>
2023-10-19 22:05:35 -0500

    Se agrega el atributo region

--- Angular/clientes-app/src/app/clientes/Cliente.ts
+++ Angular/clientes-app/src/app/clientes/Cliente.ts
@@ -1,3 +1,5 @@
+import { Region } from "./region";
+
 export class Cliente {
    id: number;
    nombre: string;
@@ -5,9 +7,10 @@ export class Cliente {
    email: string;
    createAt: string;
    foto: string;
+   region: Region;
 
    // Los parametros con ? indica que son opcionales
-   constructor(id?: number, nombre?: string, apellido?: string, email?: string, createAt?: string,foto?:string) {
+   constructor(id?: number, nombre?: string, apellido?: string, email?: string, createAt?: string,foto?:string, region?: Region) {
       // Si no se envia recibe un valor, se asigna un valor por defecto
       this.id = id || 0;
       this.nombre = nombre || '';
@@ -15,5 +18,6 @@ export class Cliente {
       this.email = email || '';
       this.createAt = createAt || '';
       this.foto=foto || '';
+      this.region = region || new Region();
    }
 }
