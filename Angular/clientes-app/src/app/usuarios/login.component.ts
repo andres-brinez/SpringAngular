db0f50ca6d32b9a33becf7ba52a2c66700746b39
andres briñez <brinezlopez08@gmail.com>
2023-10-25 18:05:24 -0500

    Añadiendo el método login() en el componente

--- Angular/clientes-app/src/app/usuarios/login.component.ts
+++ Angular/clientes-app/src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +11,23 @@ export class LoginComponent {
 
 
   titulo: string = 'Por favor Sign In!';
+  usuario: Usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  login(): void {
+    console.log(this.usuario);
+
+    // Validar que el usuario y la contraseña no estén vacíos con swal
+    if (this.usuario.username == "" || this.usuario.password == "") {
+      // Se muestra un mensaje de error
+       Swal.fire('Error Login', 'Username o password vacías!', 'error');
+      return;
+    }
+    
+
+  }
 
 }
