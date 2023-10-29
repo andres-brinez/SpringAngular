11b72c654ed89ee4b5ac354be73424d00b6c87c2
andres briñez <brinezlopez08@gmail.com>
2023-10-29 12:26:46 -0500

    Añadiendo campos al formulario factura

--- Angular/clientes-app/src/app/facturas/facturas.component.ts
+++ Angular/clientes-app/src/app/facturas/facturas.component.ts
@@ -10,19 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 export class FacturasComponent implements OnInit {
 
   titulo: string = 'Nueva Factura';
-  factura:Factura = new Factura();
+  factura: Factura = new Factura();
 
-  constructor(private clienteService:ClienteService,private activateRoute:ActivatedRoute) { }
-  
+  constructor(private clienteService: ClienteService, private activateRoute: ActivatedRoute) { }
+
   ngOnInit(): void {
     /* params es una forma de acceder a los parámetros de la URL de forma sincrónica,
      mientras que paramMap es una forma de acceder a los parámetros de la URL de forma asíncrona mediante un observable. */
-    this.activateRoute.paramMap.subscribe(params => {
-      let clienteId = +params.get('clienteId') ! | -1; // Se obtiene el parámetro clienteId de la url y Asigna -1 si clienteId es null 
-      if(clienteId != -1){
+    this.activateRoute.params.subscribe(params => {
+      let clienteId = params['clienteId'];
+      if (clienteId != null) {
+        console.log(clienteId);
         this.clienteService.getCliente(clienteId).subscribe(
           cliente => this.factura.cliente = cliente // Se asigna el cliente que se obtiene del servicio al cliente de la factura
         );
+        // console.log(this.factura);
+
       }
     });
   }
